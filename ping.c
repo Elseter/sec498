99c5984e3c93831be113cadcdb05fe9279f82014
Elseter <rking3425@gmail.com>
2025-04-11 09:46:43 -0400

    Initialize Repo

---
+++ ping.c
@@ -0,0 +1,5 @@
+//
+// Created by Riley King on 4/11/25.
+//
+
+#include "ping.h"
---
+++ ping.h
@@ -0,0 +1,8 @@
+//
+// Created by Riley King on 4/11/25.
+//
+
+#ifndef SEC498_PING_H
+#define SEC498_PING_H
+
+#endif //SEC498_PING_H
